90c07bcaaed6f95fe82b1357ea983e3dff2f8de4
Alex <>
2022-06-13 12:02:37 -0400

    [~] Implement responsive design

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,11 @@ import { Details, Cart } from "../src/sections";
 const Home: NextPage = () => {
   return (
     <Container maxW="container.xl" p={0}>
-      <Flex h="100vh" py={20}>
+      <Flex
+        h={{ base: "auto", md: "100vh" }}
+        py={[0, 10, 20]}
+        direction={{ base: "column-reverse", md: "row" }}
+      >
         <Details />
         <Cart />
       </Flex>
